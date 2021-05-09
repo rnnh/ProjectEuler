4260ca98293e75b2b72fcd45ffb43976fe3641a5
rnnh <r.harrington747@gmail.com>
2021-05-09 20:30:30 +0100

    Added Python/pe_0001.py

---
+++ Python/pe_0001.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Find the sum of all the multiples of 3 or 5 below 1000
+
+multiples = []
+
+for i in range(1,1000):
+    if i % 3 == 0:
+        multiples.append(i)
+    elif i % 5 == 0:
+        multiples.append(i)
+
+print(sum(multiples))
