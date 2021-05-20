1de5de3e0ebfd8c5da55d4a96a9f0ac43d99016f
Ronan Harrington <57678111+rnnh@users.noreply.github.com>
2021-05-20 22:30:45 +0100

    Added Python/pe_0003.py

---
+++ Python/pe_0003.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def find_largest_prime_factor(n):
+    while n % 2 == 0 and n / 2 > 1:
+        n = n / 2
+    for i in range(3, int((n ** 0.5) + 1), 2):
+        while n % i == 0 and n / i != 1:
+            n = n / i
+    print(int(n))
+
+find_largest_prime_factor(600851475143)
