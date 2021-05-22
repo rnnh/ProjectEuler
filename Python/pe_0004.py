a5c962db994fe6980d7b69651d03b13fba00f823
Ronan Harrington <57678111+rnnh@users.noreply.github.com>
2021-05-22 13:43:08 +0100

    Added Python/pe_0004.py

---
+++ Python/pe_0004.py
@@ -0,0 +1,23 @@
+#! /usr/bin/python3
+
+def find_largest_palindrome():
+	largest_palindrome = 0
+	for x in range(999, 99, -1):
+		for y in range(999, 750, -1):
+			product = x * y
+			product_reversed = int(str(product)[::-1])
+			if product > largest_palindrome and product == product_reversed:
+				largest_palindrome = product
+			if product < largest_palindrome and product == product_reversed:
+				return(largest_palindrome)
+	for x in range(999, 99, -1):
+		for y in range(751, 99, -1):
+			product = x * y
+			product_reversed = int(str(product)[::-1])
+			if product > largest_palindrome and product == product_reversed:
+				largest_palindrome = product
+			if product < largest_palindrome and product == product_reversed:
+				return(largest_palindrome)
+
+largest_three_digit_palindrome = find_largest_palindrome()
+print(largest_three_digit_palindrome)
