95e0c2ed2049d889ddb4cc5494507e6ef1eff77d
Lint Action <lint-action@samuelmeuli.com>
2021-07-14 15:50:56 +0000

    Fix code style issues with Black

--- Python/pe_0006.py
+++ Python/pe_0006.py
@@ -2,8 +2,8 @@
 
 
 def find_sum_square_difference(n):
-    sum_of_integers = n * (n + 1)/2
-    sum_of_squares = (n + 1) * (2 * n + 1) * n/6
+    sum_of_integers = n * (n + 1) / 2
+    sum_of_squares = (n + 1) * (2 * n + 1) * n / 6
     print(sum_of_integers ** 2 - sum_of_squares)
 
 
