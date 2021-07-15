b3db79a71a8a51c4c21510aec3d5885a1e037db3
Ronan Harrington <57678111+rnnh@users.noreply.github.com>
2021-07-15 17:37:13 +0100

    Added Python/pe_0007.py

---
+++ Python/pe_0007.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+def find_nth_prime(n):
+    primes = [2]
+    candidate = 2
+    is_prime = True
+    while len(primes) <= n - 1:
+        for prime in primes:
+            if candidate % prime == 0:
+                    is_prime = False
+                    break
+        if is_prime:
+            primes += [candidate]
+        candidate += 1
+        is_prime = True
+    return primes[-1]
+
+
+prime_10001 = find_nth_prime(10001)
+
+print(prime_10001)
