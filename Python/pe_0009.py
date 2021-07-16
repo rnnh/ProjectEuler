caf674b01e3c340628862bf13b99ca5ed6e04521
Ronan Harrington <57678111+rnnh@users.noreply.github.com>
2021-07-16 16:47:26 +0100

    Added Python/pe_0009.py

---
+++ Python/pe_0009.py
@@ -0,0 +1,21 @@
+#! /bin/usr/python3
+
+
+def find_pythagorean_triplet_product():
+    a = 0
+    b = 1
+    c = 2
+    while a + b + c != 1000:
+        a += 1
+        b = (1000 ** 2 - 2000 * a) / (2000 - 2 * a)
+        if isinstance(b, float) or a > b:
+            break
+        c = (1000 ** 2 + 2 * b * (b - 1000)) / (2000 - 2 * b)
+        if isinstance(c, float) or b > c:
+            break
+    return a * b * c
+
+
+pythagorean_triplet_product = find_pythagorean_triplet_product()
+
+print(pythagorean_triplet_product)
