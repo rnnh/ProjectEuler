dce9b03156dae67a672be92a579bc8c8575aa113
Ronan Harrington <57678111+rnnh@users.noreply.github.com>
2021-10-10 14:32:31 +0100

    Changed variable names for clarity

--- Python/pe_0012.py
+++ Python/pe_0012.py
@@ -7,22 +7,22 @@ def find_triangle_number(n_factors: int):
     factors = []
     lower_half = []
     upper_half = []
-    term = 1
-    next_term = 2
+    triangle = 1
+    next_number = 2
     while len(factors) < n_factors:
         factors = []
         lower_half = []
         upper_half = []
-        term += next_term
-        next_term += 1
-        for x in range(1, int(sqrt(term)) + 1):
-            if term % x == 0:
+        triangle += next_number
+        next_number += 1
+        for x in range(1, int(sqrt(triangle)) + 1):
+            if triangle % x == 0:
                 lower_half += [x]
         for y in lower_half:
-            if term % y == 0:
-                upper_half += [term / y]
+            if triangle % y == 0:
+                upper_half += [triangle / y]
         factors = list(set(lower_half + upper_half))
-    return term
+    return triangle
 
 
 triangle_500 = find_triangle_number(500)
