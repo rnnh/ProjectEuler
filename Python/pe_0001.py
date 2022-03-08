9cb5fe693c817133b78e99c260f812c34ae69950
Lint Action <lint-action@samuelmeuli.com>
2022-03-08 21:09:21 +0000

    Fix code style issues with Black

--- Python/pe_0001.py
+++ Python/pe_0001.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 
+
 def main():
     # Find the sum of all the multiples of 3 or 5 below 1000
-    
+
     multiples = []
-    
+
     for i in range(1, 1000):
         if i % 3 == 0:
             multiples.append(i)
         elif i % 5 == 0:
             multiples.append(i)
-    
+
     print(sum(multiples))
+
 
 if __name__ == "__main__":
     main()
