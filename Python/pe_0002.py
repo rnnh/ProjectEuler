526d530c89a64c7b05dd04050e15652bae8ae153
Lint Action <lint-action@samuelmeuli.com>
2022-03-08 21:17:16 +0000

    Fix code style issues with Black

--- Python/pe_0002.py
+++ Python/pe_0002.py
@@ -4,15 +4,15 @@
 def main():
     fibonacci = [1, 2]
     even_terms_sum = 0
-    
+
     while fibonacci[-2] + fibonacci[-1] < 4000000:
         new_term = fibonacci[-2] + fibonacci[-1]
         fibonacci.append(new_term)
-    
+
     for term in fibonacci:
         if term % 2 == 0:
             even_terms_sum += term
-    
+
     print(even_terms_sum)
 
 
