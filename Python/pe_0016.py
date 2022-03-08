83192b920462b40924e47f2cd399ac09042f26ad
Lint Action <lint-action@samuelmeuli.com>
2022-03-08 20:07:30 +0000

    Fix code style issues with Black

--- Python/pe_0016.py
+++ Python/pe_0016.py
@@ -8,12 +8,12 @@ def find_sum_of_digits(n: int):
         digit_sum += int(i)
     return digit_sum
 
-  
+
 def find_exponent_sum(base: int, exponent: int):
     power = base**exponent
     power_digit_sum = find_sum_of_digits(power)
     return power_digit_sum
 
-  
+
 two_power_thousand_sum = find_exponent_sum(base=2, exponent=1000)
 print(two_power_thousand_sum)
