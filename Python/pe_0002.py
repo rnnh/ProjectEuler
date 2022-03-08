9f888326bf793089e22786b01f433a358b1ac295
Ronan Harrington <ronan_harr@protonmail.com>
2022-03-08 21:16:18 +0000

    Added main() to Python/pe_0002.py

--- Python/pe_0002.py
+++ Python/pe_0002.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 
-fibonacci = [1, 2]
-even_terms_sum = 0
 
-while fibonacci[-2] + fibonacci[-1] < 4000000:
-    new_term = fibonacci[-2] + fibonacci[-1]
-    fibonacci.append(new_term)
+def main():
+    fibonacci = [1, 2]
+    even_terms_sum = 0
+    
+    while fibonacci[-2] + fibonacci[-1] < 4000000:
+        new_term = fibonacci[-2] + fibonacci[-1]
+        fibonacci.append(new_term)
+    
+    for term in fibonacci:
+        if term % 2 == 0:
+            even_terms_sum += term
+    
+    print(even_terms_sum)
 
-for term in fibonacci:
-    if term % 2 == 0:
-        even_terms_sum += term
 
-print(even_terms_sum)
+if __name__ == "__main__":
+    main()
